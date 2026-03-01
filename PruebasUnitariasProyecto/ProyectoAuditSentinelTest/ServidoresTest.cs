ec3c929a90185851ec84c14bd3891d563bc1574d
HP <HP@DESKTOP-2IS4L76>
2026-03-01 17:20:18 -0500

    [ADD] Adjuntando Pruebas unitarias

---
+++ PruebasUnitariasProyecto/ProyectoAuditSentinelTest/ServidoresTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoAuditSentinel;
+using Xunit;
+using FluentAssertions;
+
+namespace ProyectoAuditSentinelTest
+{
+    public class ServidoresTest 
+    {
+        private readonly Servidores _servidores = new Servidores();
+        [Fact]
+
+        public void Servidores_Should_Have_Required_Properties()
+        {
+            // Arrange
+            var servidores = new Servidores
+            {
+                IdServidor = 1,
+                NombreServidor = "SDC-SWE2",
+                IP = "192.168.1.10",
+                SistemaOperativo = SistemaOperativo.WindowsServer2012,
+                Create_is = DateTime.UtcNow
+            };
+
+            // Act & Assert
+            servidores.Should().NotBeNull();
+            servidores.IdServidor.Should().BeGreaterThan(0);
+            servidores.NombreServidor.Should().NotBeNullOrEmpty();
+            servidores.IP.Should().NotBeNullOrEmpty();
+            servidores.SistemaOperativo.Should().Be(SistemaOperativo.WindowsServer2012);
+            servidores.Create_is.Should().BeOnOrAfter(DateTime.MinValue);
+
+
+        }
+    }
+}
