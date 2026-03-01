80985bfb2854b199c788bb7711e6ee31f8195480
giovannyortegon <giovanny.ortegon@gmail.com>
2026-03-01 13:59:03 -0500

    [ADD] agregando enviarmensaje

--- AuditSentinel/AuditSentinel/Data/ApplicationDBContext.cs
+++ AuditSentinel/AuditSentinel/Data/ApplicationDBContext.cs
@@ -22,8 +22,8 @@ namespace AuditSentinel.Data
         public DbSet<AuditSentinel.Models.Reportes> Reportes { get; set; }
         public DbSet<AuditSentinel.Models.EscaneosReportes> EscaneosReportes { get; set; }
         public DbSet<AuditSentinel.Models.EscaneosVulnerabilidades> EscaneosVulnerabilidades { get; set; }
-
         public DbSet<LogErroresEscaneo> LogErroresEscaneos { get; set; }
+        public DbSet<AuditSentinel.Models.Correo> Correos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
