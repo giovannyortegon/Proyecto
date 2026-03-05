9c6c8db9a3207266515ada689e01405f821bee3c
giovannyortegon <giovanny.ortegon@gmail.com>
2026-03-04 20:06:03 -0500

    [UPDATE] Servidores para que no repita ni por nombre ni por ip

--- AuditSentinel/AuditSentinel/Pages/Servidores/Create.cshtml.cs
+++ AuditSentinel/AuditSentinel/Pages/Servidores/Create.cshtml.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AuditSentinel.Data;
+using AuditSentinel.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using AuditSentinel.Data;
-using AuditSentinel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AuditSentinel.Pages.Servidores
 {
@@ -30,15 +31,26 @@ namespace AuditSentinel.Pages.Servidores
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    return Page();
-            //}
+            
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Verificar si ya existe un servidor con la misma clave primaria o campo único
+            var existe = await _context.Servidores.AnyAsync(s => s.NombreServidor == Servidores.NombreServidor || s.IP == Servidores.IP);
+  
+            if (existe)
+            {
+                ModelState.AddModelError(string.Empty, "El servidor ya existe en la base de datos.");
+                return Page(); // Regresa a la página mostrando el error
+            }
 
             _context.Servidores.Add(Servidores);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+
         }
     }
 }
