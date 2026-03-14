c6000fb038999aa540a8f8a4e963b1f9e739b392
HP <HP@DESKTOP-2IS4L76>
2026-03-14 08:42:40 -0500

    deleted:    ../../PruebasUnitariasProyecto/ProyectoAuditSentinelTest/ProyectoAuditSentinelTest.csproj 	modified:   ../../PruebasUnitariasProyecto/ProyectoAuditSentinelTest/ServidoresTest.cs 	deleted:    ../../PruebasUnitariasProyecto/Servidores/ProyectoAuditSentinel.csproj 	../../.gitignore.txt 	../../.vs/ 	../.gitignore.txt 	../.vs/ 	.vs/ 	Migrations/ 	../../PruebasUnitariasProyecto/.gitignore.txt 	../../PruebasUnitariasProyecto/.vs/ 	../../PruebasUnitariasProyecto/ProyectoAuditSentinelTest/AuditSentinel.Test.csproj 	../../PruebasUnitariasProyecto/ProyectoAuditSentinelTest/bin/ 	../../PruebasUnitariasProyecto/ProyectoAuditSentinelTest/obj/ 	../../PruebasUnitariasProyecto/PruebasUnitariasProyecto.slnx 	../../PruebasUnitariasProyecto/Servidores/AuditSentinel.csproj 	../../PruebasUnitariasProyecto/Servidores/bin/ 	../../PruebasUnitariasProyecto/Servidores/obj/

--- PruebasUnitariasProyecto/ProyectoAuditSentinelTest/ServidoresTest.cs
+++ PruebasUnitariasProyecto/ProyectoAuditSentinelTest/ServidoresTest.cs
@@ -6,14 +6,41 @@ using System.Threading.Tasks;
 using ProyectoAuditSentinel;
 using Xunit;
 using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoAuditSentinelTest
 {
     public class ServidoresTest 
     {
+        private static(bool valido, List<ValidationResult> results) Validar(object model)
+                    {
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+            var valido = Validator.TryValidateObject(model, context, results, true);
+            return (valido, results);
+        }
         private readonly Servidores _servidores = new Servidores();
-        [Fact]
+        [Theory]
+        [InlineData("Servidor1", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        [InlineData("   ", false)]
+        [InlineData(" Servidor1",false)]
+        public void RegistroNombreObligatorio(string nombre, bool ValidadorEsperado)
+        {
+            // Arrange
+            var model = new Servidores
+            {
+                IdServidor = 1,
+                NombreServidor = nombre, // NombreServidor es obligatorio
+                IP = "192.168.1.10",
+                SistemaOperativo = SistemaOperativo.WindowsServer2012,
+                Create_is = DateTime.UtcNow
+            };
 
+            var (valido, _) = Validar(model);
+            Assert.Equal(ValidadorEsperado, valido);
+        }
         public void Servidores_Should_Have_Required_Properties()
         {
             // Arrange
@@ -33,8 +60,47 @@ namespace ProyectoAuditSentinelTest
             servidores.IP.Should().NotBeNullOrEmpty();
             servidores.SistemaOperativo.Should().Be(SistemaOperativo.WindowsServer2012);
             servidores.Create_is.Should().BeOnOrAfter(DateTime.MinValue);
+         }
 
+        [Fact]
+        public void Servidores_Should_Allow_Valid_IP_Address()
+        {
+            // Arrange
+            var servidor = new Servidores
+            {
+                IdServidor = 2,
+                NombreServidor = "Servidor01",
+                IP = "192.168.0.1",
+                SistemaOperativo = SistemaOperativo.WindowsServer2012,
+                Create_is = DateTime.UtcNow
+            };
 
+            // Act
+            var ip = servidor.IP;
+
+            // Assert
+            ip.Should().Contain(".");
+            ip.Should().NotBeNullOrEmpty();
+        }
+        [Fact]
+        public void Servidores_Should_Have_Server_Name()
+        {
+            // Arrange
+            var servidor = new Servidores
+            {
+                IdServidor = 3,
+                NombreServidor = "ServidorProduccion",
+                IP = "10.0.0.5",
+                SistemaOperativo = SistemaOperativo.WindowsServer2012,
+                Create_is = DateTime.UtcNow
+            };
+
+            // Act
+            var nombre = servidor.NombreServidor;
+
+            // Assert
+            nombre.Should().NotBeNullOrWhiteSpace();
+            nombre.Length.Should().BeGreaterThan(3);
         }
     }
 }
